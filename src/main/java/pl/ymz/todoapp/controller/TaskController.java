81d31e6a8251a78188ce9ff190dd221894790878
Michał Mielczarek <michal.mielczarek@gmail.com>
2020-04-17 13:41:53 +0200

    Udemy/Java - Spring Framework #37. HTTP PUT mapping

--- src/main/java/pl/ymz/todoapp/controller/TaskController.java
+++ src/main/java/pl/ymz/todoapp/controller/TaskController.java
@@ -5,10 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import pl.ymz.todoapp.model.Task;
 import pl.ymz.todoapp.model.TaskRepository;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -21,18 +24,22 @@ class TaskController {
         this.repository = repository;
     }
 
-    //    @RequestMapping(method = RequestMethod.GET, path = "/tasks")
     @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
-    //taka adnotacja zamiast tej wyżej jeśli i tak metoda GET
     ResponseEntity<List<Task>> readAllTasks() {
         logger.warn("Kontroler z /tasks z wyłączeniem parametrów sort page i size");
         return ResponseEntity.ok(repository.findAll());
     }
 
     @GetMapping("/tasks")
-        //taka adnotacja zamiast tej wyżej jeśli i tak metoda GET
     ResponseEntity<List<Task>> readAllTasks(Pageable page) {
         logger.info("Kontroler z parametrami stron");
         return ResponseEntity.ok(repository.findAll(page).getContent());
     }
+
+    @PutMapping("/tasks/{id}")
+    ResponseEntity<?> updateTask(@RequestBody @Valid Task toUpdate) {
+        repository.save(toUpdate);
+        return ResponseEntity.noContent().build();
+    }
+
 }
