f5d3b59d1d7b73325c21e682b9ecf076946fcec1
YMnZSdlc <michal.mielczarek@gmail.com>
2020-04-20 00:41:31 +0200

    Udemy/Java - Spring Framework #49. Własne konfiguracje w działaniu

--- src/main/java/pl/ymz/todoapp/controller/InfoController.java
+++ src/main/java/pl/ymz/todoapp/controller/InfoController.java
@@ -1,26 +1,32 @@
 package pl.ymz.todoapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.ymz.todoapp.TaskConfigurationProperties;
 
 @RestController
 @RequestMapping("/info")
 class InfoController {
 
-    @Value("${spring.datasource.url}")
-    private String url;
-    @Value("${my.prop}")
-    private String myProp;
+    private DataSourceProperties dataSource;
+    private TaskConfigurationProperties myProp;
+
+    public InfoController(DataSourceProperties dataSource, TaskConfigurationProperties myProp) {
+        this.dataSource = dataSource;
+        this.myProp = myProp;
+    }
 
     @GetMapping("/url")
     String url(){
-        return url;
+        return dataSource.getUrl();
     }
 
     @GetMapping("/prop")
-    String myProp(){
-        return myProp;
+    boolean myProp(){
+        return myProp.isAllowMultipleTasksFromTemplate();
     }
 }
