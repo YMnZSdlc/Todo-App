038606ac3559152a4537662566ef780aeee5e80e
YMnZSdlc <michal.mielczarek@gmail.com>
2020-04-25 16:30:43 +0200

    Udemy/Java - Spring Framework #80. Spring Data: @ManyToOne

--- src/main/java/pl/ymz/todoapp/model/SqlTaskRepository.java
+++ src/main/java/pl/ymz/todoapp/model/SqlTaskRepository.java
@@ -8,7 +8,13 @@ import org.springframework.stereotype.Repository;
 @Repository
 interface SqlTaskRepository extends TaskRepository, JpaRepository<Task, Integer> {
 
+    //Przykład własnego zapytania SQL do bazy
     @Override
     @Query(nativeQuery = true, value = "select count(*)>0 from TASKS where id=:id")
     boolean existsById(@Param("id") Integer id);
+
+    //Typowe zapytanie Spring Data
+    @Override
+    boolean existsByDoneIsFalseAndGroup_Id(Integer groupId);
+
 }
