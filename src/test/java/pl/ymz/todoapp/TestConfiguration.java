77ac0a8c7999416123c34e35d2d84e1d04a18464
Michał Mielczarek <michal.mielczarek@gmail.com>
2020-05-08 15:53:17 +0200

    Udemy/Java - Spring Framework #112. @Qualifier, @Primary, @ConditionalOn

---
+++ src/test/java/pl/ymz/todoapp/TestConfiguration.java
@@ -0,0 +1,61 @@
+package pl.ymz.todoapp;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import pl.ymz.todoapp.model.Task;
+import pl.ymz.todoapp.rpository.TaskRepository;
+
+import java.util.*;
+
+@Configuration
+class TestConfiguration {
+
+    @Bean
+//    @Primary
+    @Profile("integration")
+    TaskRepository testRepo() {
+        return new TaskRepository() {
+            private Map<Integer, Task> tasks = new HashMap<>();
+
+            @Override
+            public List<Task> findAll() {
+                return new ArrayList<>(tasks.values());
+            }
+
+            @Override
+            public Page<Task> findAll(Pageable page) {
+                return null;
+            }
+
+            @Override
+            public Optional<Task> findById(Integer id) {
+                return Optional.ofNullable(tasks.get(id));
+            }
+
+            @Override
+            public boolean existsById(Integer id) {
+                return tasks.containsKey(id);
+            }
+
+            @Override
+            public boolean existsByDoneIsFalseAndGroup_Id(Integer groupId) {
+                return false;
+            }
+
+            @Override
+            public Task save(Task entity) {
+                return tasks.put(tasks.size() + 1, entity);
+            }
+
+            @Override
+            public List<Task> findByDone(boolean done) {
+                return null;
+            }
+        };
+    }
+}
