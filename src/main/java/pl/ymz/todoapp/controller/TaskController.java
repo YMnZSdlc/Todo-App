d45d2610cbbe937a3c71ff68fdf1facf5bc0f7d8
YMnZSdlc <michal.mielczarek@gmail.com>
2020-05-15 19:16:39 +0200

    Udemy/Java - Spring Framework #130. @RequestMapping na klasie

--- src/main/java/pl/ymz/todoapp/controller/TaskController.java
+++ src/main/java/pl/ymz/todoapp/controller/TaskController.java
@@ -3,6 +3,7 @@ package pl.ymz.todoapp.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.ymz.todoapp.model.Task;
@@ -14,6 +15,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
+@RequestMapping("/tasks")
 class TaskController {
 
     private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
@@ -23,27 +25,46 @@ class TaskController {
         this.taskRepository = taskRepository;
     }
 
-    @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
+    @PostMapping
+    ResponseEntity<Task> createTask(@RequestBody @Valid Task taskToCreate) {
+        logger.info("Próba utworzenia zadania");
+        Task task = taskRepository.save(taskToCreate);
+        logger.info("Utworzono zadanie o nr ID:" + taskToCreate.getId());
+        return ResponseEntity.created(URI.create("/" + task.getId()))
+                .body(task);
+    }
+
+    @GetMapping(params = {"!sort", "!page", "!size"})
     ResponseEntity<List<Task>> readAllTasks() {
         logger.warn("Kontroler z /tasks z wyłączeniem parametrów sort page i size");
         return ResponseEntity.ok(taskRepository.findAll());
     }
 
-    @GetMapping("/tasks")
+    @GetMapping
     ResponseEntity<List<Task>> readAllTasks(Pageable page) {
         logger.info("Kontroler z parametrami stron");
         return ResponseEntity.ok(taskRepository.findAll(page).getContent());
     }
 
-    @GetMapping("/tasks/{id}")
-    ResponseEntity<Task> readTaskById(@PathVariable int id) {
+    @GetMapping("/{id}")
+    ResponseEntity<Task> readTask(@PathVariable int id) {
         logger.info("Odczyt konkretnego zadania po ID:" + id);
         return taskRepository.findById(id)
                 .map(task -> ResponseEntity.ok(task))
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    @PutMapping("/tasks/{id}")
+    @GetMapping(value = "/search/done", produces = MediaType.APPLICATION_JSON_VALUE)
+    String foo(){
+        return "foo - jason";
+    }
+
+    @GetMapping(value = "/search/done", produces = MediaType.TEXT_XML_VALUE)
+    String bar(){
+        return "bar - xml";
+    }
+
+    @PutMapping("/{id}")
     ResponseEntity<?> updateTask(@PathVariable("id") int taskId, @RequestBody @Valid Task taskToUpdate) {
         if (!taskRepository.existsById(taskId)) {
             return ResponseEntity.notFound().build();
@@ -57,7 +78,7 @@ class TaskController {
     }
 
     @Transactional
-    @PatchMapping("/tasks/{id}")
+    @PatchMapping("/{id}")
     public ResponseEntity<?> toggleTask(@PathVariable int id) {
         if (!taskRepository.existsById(id)) {
             return ResponseEntity.notFound().build();
@@ -65,14 +86,5 @@ class TaskController {
         taskRepository.findById(id)
                 .ifPresent(task -> task.setDone(!task.isDone()));
         return ResponseEntity.noContent().build();
-    }
-
-    @PostMapping("/tasks")
-    ResponseEntity<Task> createTask(@RequestBody @Valid Task taskToCreate) {
-        logger.info("Próba utworzenia zadania");
-        Task task = taskRepository.save(taskToCreate);
-        logger.info("Utworzono zadanie o nr ID:" + taskToCreate.getId());
-        return ResponseEntity.created(URI.create("/" + task.getId()))
-                .body(task);
     }
 }
