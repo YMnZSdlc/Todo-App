0f9affe4905b67435bbc047749bf4335a2033346
YMnZSdlc <michal.mielczarek@gmail.com>
2020-05-17 22:04:35 +0200

    Udemy/Java - Spring Framework #138. Wstrzyknięcie wszystkich interceptorów (wstrzykiwanie kolekcji)

--- src/main/java/pl/ymz/todoapp/controller/LoggerInterceptor.java
+++ src/main/java/pl/ymz/todoapp/controller/LoggerInterceptor.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,13 +19,5 @@ class LoggerInterceptor implements HandlerInterceptor {
                              Object handler) throws Exception {
         logger.info("[preHandle] " + request.getMethod() + " " + request.getRequestURI());
         return true;
-    }
-
-    @Override
-    public void postHandle(HttpServletRequest request,
-                           HttpServletResponse response,
-                           Object handler,
-                           ModelAndView modelAndView) throws Exception {
-        logger.info("[postHandle]");
     }
 }
