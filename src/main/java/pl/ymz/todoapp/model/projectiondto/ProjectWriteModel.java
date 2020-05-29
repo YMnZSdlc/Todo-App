73e9f186ed140b456d66ec0788b2fd7d7787d604
YMnZSdlc <michal.mielczarek@gmail.com>
2020-05-29 11:13:17 +0200

    Udemy/Java - Spring Framework #154. Zagnieżdżone obiekty w formularzu

--- src/main/java/pl/ymz/todoapp/model/projectiondto/ProjectWriteModel.java
+++ src/main/java/pl/ymz/todoapp/model/projectiondto/ProjectWriteModel.java
@@ -5,6 +5,7 @@ import pl.ymz.todoapp.model.ProjectStep;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
@@ -13,7 +14,11 @@ public class ProjectWriteModel {
     @NotBlank(message = "Opis szablonu nie może być pusty")
     private String description;
     @Valid
-    private List<ProjectStep> steps;
+    private List<ProjectStep> steps = new ArrayList<>();
+
+    public ProjectWriteModel() {
+        steps.add(new ProjectStep());
+    }
 
     public String getDescription() {
         return description;
