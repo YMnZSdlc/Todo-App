f8ef811fd25cc75304193aa5b6bbe1b1ac6665cf
YMnZSdlc <michal.mielczarek@gmail.com>
2020-06-01 17:21:07 +0200

    Udemy/Java - Spring Framework #157. @ModelAttribute

--- src/main/java/pl/ymz/todoapp/controller/ProjectController.java
+++ src/main/java/pl/ymz/todoapp/controller/ProjectController.java
@@ -5,7 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.ymz.todoapp.model.ProjectStep;
 import pl.ymz.todoapp.model.projectiondto.ProjectWriteModel;
 
 @Controller
@@ -21,5 +24,12 @@ public class ProjectController {
         model.addAttribute("project", projectToEdit);
         return "projects";
     }
+
+    @PostMapping(params = "addStep")
+    String addProjectStep (@ModelAttribute("project") ProjectWriteModel current) {
+        current.getSteps().add(new ProjectStep());
+        return "projects";
+    }
+
 
 }
