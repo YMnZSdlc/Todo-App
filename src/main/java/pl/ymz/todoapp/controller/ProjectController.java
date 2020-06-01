2da15a20a324fcde1298c282a5a47f77ba4c8f8e
YMnZSdlc <michal.mielczarek@gmail.com>
2020-06-01 17:39:55 +0200

    Udemy/Java - Spring Framework #158. Zapis przez formularz, th:if

--- src/main/java/pl/ymz/todoapp/controller/ProjectController.java
+++ src/main/java/pl/ymz/todoapp/controller/ProjectController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.ymz.todoapp.logicservice.ProjectService;
 import pl.ymz.todoapp.model.ProjectStep;
 import pl.ymz.todoapp.model.projectiondto.ProjectWriteModel;
 
@@ -16,18 +17,34 @@ import pl.ymz.todoapp.model.projectiondto.ProjectWriteModel;
 public class ProjectController {
 
     private static final Logger logger = LoggerFactory.getLogger(ProjectController.class);
+    private final ProjectService projectService;
+
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
 
     @GetMapping
     String showProjects(Model model) {
         var projectToEdit = new ProjectWriteModel();
         projectToEdit.setDescription("testowy opis");
         model.addAttribute("project", projectToEdit);
+        logger.info("Odczyt Projektu");
+        return "projects";
+    }
+
+    @PostMapping
+    String addProject(@ModelAttribute("project") ProjectWriteModel current, Model model){
+        projectService.save(current);
+        model.addAttribute("project", new ProjectWriteModel());
+        model.addAttribute("message", "Dodano projekt");
+        logger.info("Dodanie projektu i potwierdzenia");
         return "projects";
     }
 
     @PostMapping(params = "addStep")
     String addProjectStep (@ModelAttribute("project") ProjectWriteModel current) {
         current.getSteps().add(new ProjectStep());
+        logger.info("Dodanie kroku w projekcie");
         return "projects";
     }
 
